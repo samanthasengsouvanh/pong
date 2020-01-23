eb285df9e0760a03704e7bfbd2c7624d2b42cbef
Samantha Sengsouvanh <samantha.sengsouvanh@gmail.com>
2020-01-23 13:34:56 -0600

    created motion keys and removed event listener

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import Paddle from "./components/Paddle";
 
 export default function App() {
+  const [p1PaddleY, setP1PaddleY] = useState(0);
+  const [p2PaddleY, setP2PaddleY] = useState(0);
+
+  function handleKey(e) {
+    const char = e.key.toLowerCase();
+    if (char === "w" || char === "s") {
+      setP1PaddleY(p1PaddleY + (char === "w" ? -10 : 10));
+    }
+    if (char === "o" || char === "l") {
+      setP2PaddleY(p2PaddleY + (char === "o" ? -10 : 10));
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("keydown", handleKey);
+    return () => window.removeEventListener("keydown", handleKey);
+  }, [p1PaddleY, p2PaddleY]);
   return (
     <div className="container">
-      <Paddle />
-      <Paddle isPlayerTwo />
+      <Paddle paddleY={p1PaddleY} />
+      <Paddle isPlayerTwo paddleY={p2PaddleY} />
     </div>
   );
 }
--- src/components/Paddle/Paddle.js
+++ src/components/Paddle/Paddle.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Paddle.css";
 
-export default function Paddle({ isPlayerTwo }) {
-  return <div className={isPlayerTwo ? "paddle player2" : "paddle"} />;
+export default function Paddle({ isPlayerTwo, paddleY }) {
+  console.log("paddleY", paddleY);
+  return (
+    <div
+      className={isPlayerTwo ? "paddle player2" : "paddle"}
+      style={{ transform: `translateY(calc(-50% + ${paddleY}px))` }}
+    />
+  );
 }
